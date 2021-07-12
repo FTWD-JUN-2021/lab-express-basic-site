c60a4ab997e9c2ab4fca1cdf6d560d41707872be
Blake Stillwell <bstillwell13@yahoo.com>
2021-07-12 17:32:34 -0400

    hello

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
-
-
-
+app.use(express.static("public"));
 
 //Home route
-app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+app.get("/", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
 
 //about route
-app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
 
 //works route
-app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
 
+app.get("/euros", (request, response, next) => {
+  response.json([
+    { Brazil: 0, Argentina: 1, Cup: "Copa America" },
+    { England: 1, Italy: 2, Cup: "Euros" },
+  ]);
+});
+
+app.listen(process.env.PORT || 5000);
